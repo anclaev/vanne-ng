176f9c0f9d3944b9d0162d1aa34ea95eea62c1ae
SAMARIN Artem <Artyom.Samarin@vaz.ru>
2022-04-07 10:59:08 +0400

    Компонент хедера.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core'
 import { NavService } from './components/nav/nav.service'
 
 import { BgVideoComponent } from './components/bg-video/bg-video.component'
+import { HeaderComponent } from './components/header/header.component'
 import { LogoComponent } from './components/logo/logo.component'
 import { NavComponent } from './components/nav/nav.component'
 
 @NgModule({
   providers: [NavService],
   imports: [CommonModule, RouterModule],
-  declarations: [BgVideoComponent, LogoComponent, NavComponent],
-  exports: [BgVideoComponent, LogoComponent, NavComponent],
+  declarations: [
+    BgVideoComponent,
+    LogoComponent,
+    NavComponent,
+    HeaderComponent,
+  ],
+  exports: [BgVideoComponent, LogoComponent, NavComponent, HeaderComponent],
 })
 export class SharedModule {}
