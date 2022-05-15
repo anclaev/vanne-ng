0d5e71af6c750292bef86b59531afd323fabcf90
anclaev <av@anclaev.com>
2022-05-15 16:42:20 +0400

    Фикс тестирования.

---
+++ src/environments/env.ts
@@ -0,0 +1,12 @@
+import { IEnvironment } from '@/common/interfaces'
+
+/**
+ * @ignore
+ */
+export const ENV: IEnvironment = {
+  ENV: 'development',
+  SENTRY_DSN:
+    'https://5fd83b96db474fa6bd96795b8bbefe9f@o1139050.ingest.sentry.io/6313915',
+  API_HOST: 'http://localhost:3000',
+  GQL_HOST: 'http://localhost:3000/gql',
+}
